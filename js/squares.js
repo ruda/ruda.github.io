2d5ffcff6ed6f4eda464f29866fd26af1e56c187
Rudá Moura <ruda.moura@gmail.com>
2019-08-03 23:24:43 -0300

    Squares

---
+++ js/squares.js
@@ -0,0 +1,50 @@
+// Copyright 2019 Rudá Moura. All rights reserved
+// The MIT License
+
+let createCanvas = function(width, height, color) {
+	let canvas = document.createElement('canvas');
+	canvas.id = color;
+	canvas.title = color;
+	canvas.width = width;
+	canvas.height = height;
+	return canvas;
+};
+
+const size = 128;
+const colors = ['aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet', 'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'grey', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightgrey', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen', 'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'purple', 'rebeccapurple', 'red', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'];
+
+let canvasElements = [];
+
+// Prepare all canvas elements
+colors.forEach(
+	function(color, index, array) {
+		canvasElements.push(createCanvas(size, size, color));
+	}
+);
+
+let create2dContext = function(canvas, color) {
+	let ctx = canvas.getContext('2d');
+	ctx.fillStyle = color;
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	return canvas.toDataURL();
+};
+
+let createImage = function(canvas) {
+	let data = create2dContext(canvas, canvas.title)
+	let img = document.createElement('img');
+	img.id = canvas.title;
+	img.title = canvas.title;
+	img.src = data;
+	return img;
+};
+
+
+// When all's well...
+window.onload = function() {
+	canvasElements.forEach(
+		function(canvas, index, array) {
+			create2dContext(canvas, canvas.title);
+			document.body.appendChild(createImage(canvas));
+		}
+	);
+};
